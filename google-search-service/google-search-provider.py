d11c7bbea1f849431e15e2f7012a54f366eb0f6e
Biju Nair <bijunair@Bijus-MacBook-Pro.local>
2019-11-20 23:15:00 +0530

    Adding search providers

--- google-search-service/google-search-provider.py
+++ google-search-service/google-search-provider.py
@@ -2,12 +2,11 @@ from confluent_kafka import KafkaError, Consumer, Producer
 import json
 import requests
 import itertools
-from bs4 import BeautifulSoup
 import urllib.parse
 
 kafkaConsumer = Consumer({
     'bootstrap.servers': 'localhost:9092',
-    'group.id': 'ddg-search-provider',
+    'group.id': 'googl-search-provider',
     'auto.offset.reset': 'earliest'
 })
 
@@ -23,9 +22,8 @@ def delivery_report(err, msg):
     else:
         print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
-URL = 'https://duckduckgo.com/html/?q='
-header = "{\"authority\": \"duckduckgo.com\", \"method\": \"POST\", \"path\": \"/html/\", \"scheme\": \"https\", \"accept\": \"text/html,application/xml,image/webp,image/apng,*/*\", \"accept-language\": \"en-US,en;q=0.9\",\"cache-control\": \"max-age=0\", \"content-length\": \"28\", \"content-type\": \"application/json\",\"cookie\": \"ax=v195-2\", \"origin\": \"https://duckduckgo.com\", \"referer\": \"https://duckduckgo.com/\", \"sec-fetch-mode\": \"navigate\", \"sec-fetch-site\": \"same-origin\",\"sec-fetch-user\": \"?1\", \"upgrade-insecure-requests\": \"1\", \"user-agent\": \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36\"}"
 
+URL = "https://www.googleapis.com/customsearch/v1?key=AIzaSyC9jWQ_P03Ad_XpavhF3qwFTeG_tZR4p4c&cx=007658124463301196615:cdxn5t6cl2e&q="
 print("Listening for new search messages to process...")
 while True:
     msg = kafkaConsumer.poll(1.0)
@@ -48,38 +46,25 @@ while True:
     if searchkey is None:
         continue
 
-    params = {'q': searchkey,'kl': 'us-en',}
-    
-    attempt = 0
-    while attempt in range(0,3):
+    while range(0,3):
         try:
-            print("Running Duck Duck Go search for '{}'".format(urllib.parse.unquote_plus(searchkey)))
-            
-            r = requests.post(URL + searchkey, data=params, headers=json.loads(header))
-            soup = BeautifulSoup(r.content, 'html5lib')
-
-            top10 = {}
-            for element in list(itertools.islice(soup.findAll('a', attrs={'class':'result__a'}),10)):
-                link = element['href']
-                top10[link] = {'title':element.get_text(), 'link': link}
-
-            for element in list(itertools.islice(soup.findAll('a', attrs={'class':'result__snippet'}),10)):
-                link = element['href']
-                top10[link]['snippet'] = element.get_text()
-
+            print("Running Google search for '{}'".format(urllib.parse.unquote_plus(searchkey)))
+            r = requests.get(url = URL + searchkey)
+            data = r.json()
+            top10 = list(itertools.islice(data["items"], 10))
+            r = list(map(lambda v:{'title':v['title'], 'link':v['link'], 'snippet':v['htmlSnippet']}, top10))
             msg = {}
-            msg['searchengine']="ddg"
+            msg['searchengine']="google"
             msg['uuid'] = uuid
             msg['keyword'] = searchkey
-            msg['results'] = json.dumps(list(top10.values()))
-
+            msg['results'] = json.dumps(r)
             # Post to Kafka
             en_msg = json.dumps(msg).encode('utf-8')
             print("Posting result to 'search-result' topic")
             kafkaProducer.produce('searchengine-result', en_msg, callback=delivery_report)
             break
         except Exception as e:
-            print("Attempt-",attempt,": Exception occured while making DDG query", e.with_traceback())
+            print("Exception occured while making google query", e)
             break
     # Fetch any available callbacks from previous posts
     kafkaProducer.flush()
---
+++ wiki-search-service/wiki-serarch-provider.py
@@ -0,0 +1,80 @@
+from confluent_kafka import KafkaError, Consumer, Producer
+import json
+import requests
+import itertools
+import wikipedia
+import urllib.parse
+
+c = Consumer({
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'wiki-search-provider',
+    'auto.offset.reset': 'earliest'
+})
+
+p = Producer({'bootstrap.servers': 'localhost:9092'})
+
+c.subscribe(['search'])
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+print("Listening for new search messages to process...")
+while True:
+    msg = c.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        print("Consumer error {}".format(msg.error()))
+    # message format sample
+    # {"searchword": "Shah%20Rukh%20Khan", "uuid":"9b7c489a-798d-4dee-a3a6-934a90fd8e99"}
+    print("New message received: ", msg.value())
+    try:
+        s = json.loads(msg.value().decode('utf-8'))
+    except Exception as e:
+        print("Malformed search query, query should format {'searchword':'search string', 'uuid':'uuid string'}")
+        continue
+    searchkey = s["searchword"]
+    
+    uuid = s["uuid"]
+
+    if searchkey is None:
+        continue
+
+    #searchkey = urllib.parse.unquote_plus(searchkey)
+
+    while range(0,3):
+        try:
+            print("Running Wiki search for '{}'".format(urllib.parse.unquote_plus(searchkey)))
+            page = wikipedia.page(searchkey)
+            r = {}
+            r["title"] = page.title
+            r["link"] = page.url
+            summary = page.summary
+            st = 0
+            for i in range(0,2):
+                st = summary.find("\n",st,len(summary))
+            r["snippet"] = summary[:st]
+            msg = {}
+            msg['searchengine']="wiki"
+            msg['uuid'] = uuid
+            msg['keyword'] = searchkey
+            msg['results'] = json.dumps([r])
+            # Post to Kafka
+            en_msg = json.dumps(msg).encode('utf-8')
+            print("Posting result to 'search-result' topic")
+            p.produce('searchengine-result', en_msg, callback=delivery_report)
+            break
+        except Exception as e:
+            print("Exception occured while making google query", e)
+            break
+
+    # Fetch any available callbacks from previous posts
+    p.flush()
+
+c.close()
